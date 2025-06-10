8de9749a4a22e30b075865ae4414c6dff242934d
IngRamirezJezhuah <ramirezalvaradodavidjezhuah@gmail.com>
2025-06-10 14:40:20 -0600

    Pacientes

--- FRONTEND/Web/integradora_2/src/App.js
+++ FRONTEND/Web/integradora_2/src/App.js
@@ -4,14 +4,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import './styles.css';
 import './Dash.css'
-import './analisis.css'
+import './plantilla.css'
 
 import Component from './Screens/Component.jsx';
-import Recuperacion from './Screens/Recuperacion.jsx';
+//import Recuperacion from './Screens/Recuperacion.jsx';
 import Dashboard from './Screens/Dashboard.jsx';
 import Pacientes from './Screens/Pacientes.jsx';
 import  Muestras  from './Screens/Muestras.jsx';
-import Login from './Screens/Login.jsx';
+//import Login from './Screens/Login.jsx';
 import Pedidos from './Screens/Pedidos.jsx';
 import Analisis from './Screens/Analisis.jsx';
 
@@ -20,20 +20,24 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* Layout principal */}
-        <Route path="/" element={<Recuperacion />}>
+        {/* Layout principal
+        <Route path="/" element={<Login />}>
 
-        {/* Páginas que usan ese layout */}
+          Páginas que usan ese layout
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Recuperacion" element={<Recuperacion />} />
+        
+        */}
 
-          <Route path="/Login" element={<Login />} />
-          <Route path="/Component" element={<Component />} />
-          <Route path="/Recuperacion" element={<Recuperacion />} />
+          
+          <Route path="/" element={<Component />} >
           <Route path="/Dashboard" element={<Dashboard />} />
           <Route path="/Pacientes" element={<Pacientes />} />
           <Route path="/Pedidos" element={<Pedidos />} />
           <Route path="/Muestras" element={<Muestras />} />
           <Route path='/Analisis' element={<Analisis />} />
-        </Route>
+          </Route>
+        {/*</Route>*/}
       </Routes>
     </Router>
   );
