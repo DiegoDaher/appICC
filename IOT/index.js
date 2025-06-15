2018ca5504ac7565af8ea142be29a8575d256839
Mario09122004 <169079274+Mario09122004@users.noreply.github.com>
2025-06-15 13:05:22 -0600

    Delete IOT directory

--- IOT/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./src/app');
-
-const config = {
-    port: process.env.PORT || 3005,
-};
-
-app.listen(config.port, () => {
-    console.log(`Servidor corriendo en el puerto ${config.port}`);
-});
