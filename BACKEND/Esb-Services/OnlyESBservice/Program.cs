1229a21728b50c96549218674f0a2f3541622ade
Mario09122004 <169079274+Mario09122004@users.noreply.github.com>
2025-06-22 14:45:10 -0600

    Integración de CORS

--- BACKEND/Esb-Services/OnlyESBservice/Program.cs
+++ BACKEND/Esb-Services/OnlyESBservice/Program.cs
@@ -2,6 +2,17 @@
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Agregar CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PermitirFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000") // <- origen del frontend
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -12,6 +23,9 @@ builder.Services.AddHttpClient(); // NECESARIO para IHttpClientFactory
 builder.Services.AddSingleton(new RedisService("token_cache:6379"));
 
 var app = builder.Build();
+
+// Usar CORS ANTES de los endpoints
+app.UseCors("PermitirFrontend");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
